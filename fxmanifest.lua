f8632647ff63eb522945ade5eb9cc173b8067a60
Mek91 <76773421+Mek91@users.noreply.github.com>
2023-08-19 06:05:20 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'mek91'
+
+description 'Mek91-RA - (Remaster AcidTrip)'
+
+version '1.0'
+
+files {
+    'index.html'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page { 
+    'index.html'
+}
+
+export 'DoAcid'
+
+lua54 'yes'
